733994917132909261f014e7eeb527ccd953cdf1
kiel barry <kiel.j.barry@gmail.com>
2018-09-17 15:59:46 -0700

    tools to use later, like why update and record

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+
+import { spring, AnimatedSwitch } from 'react-router-transition';
 // import cn from 'classnames';
 
 // actions
@@ -105,18 +107,48 @@ class App extends Component {
   }
 
   render() {
+    // we need to map the `scale` prop we define below
+    // to the transform style property
+    function mapStyles(styles) {
+      return { opacity: styles.opacity, transform: `scale(${styles.scale})` };
+    }
+    // wrap the `spring` helper to use a bouncy config
+    function bounce(val) {
+      return spring(val, { stiffness: 330, damping: 22 });
+    }
+    // child matches will...
+    const bounceTransition = {
+      // start in a transparent, upscaled state
+      atEnter: { opacity: 0, scale: 1.2 },
+      // leave in a transparent, downscaled state
+      atLeave: { opacity: bounce(0), scale: bounce(0.8) },
+      // and rest at an opaque, normally-scaled state
+      atActive: { opacity: bounce(1), scale: bounce(1) },
+    };
     return (
       <BrowserRouter>
         <div>
           <div className="App">
             <NavBar />
+
             <div className="dapp-flex-content">
               <main className="dapp-content">
                 <div>
-                  <Route path="/account/*" component={SingleAccountView} />
-                  <Route exact path="/" component={AccountView} />
-                  <Route exact path="/send-from" component={SendContractForm} />
-                  <Route exact path="/contracts" component={ContractsView} />
+                  <AnimatedSwitch
+                    atEnter={bounceTransition.atEnter}
+                    atLeave={bounceTransition.atLeave}
+                    atActive={bounceTransition.atActive}
+                    mapStyles={mapStyles}
+                  >
+                    <Route path="/account/*" component={SingleAccountView} />
+                    <Route exact path="/" component={AccountView} />
+                    <Route
+                      exact
+                      path="/send-from"
+                      component={SendContractForm}
+                    />
+                    <Route exact path="/contracts" component={ContractsView} />
+                  </AnimatedSwitch>
                 </div>
                 <MistAlertBubble />
               </main>
--- src/index.js
+++ src/index.js
@@ -3,20 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-// import { Provider, connect } from  'react-redux';
+import ErrorBoundary from './errorBoundaries/ErrorBoundary';
 import { Provider } from 'react-redux';
-
 import store from './store/store.js';
-
 // web3Init
 import getWeb3 from './web3/getWeb3.js';
+require('dotenv').config();
 
 getWeb3.catch(err => console.warn('Error in web3 initialization.', err));
 
+if (process.env.NODE_ENV === 'development') {
+  // const {whyDidYouUpdate} = require('why-did-you-update');
+  // whyDidYouUpdate(React);
+}
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </Provider>,
   document.getElementById('root')
 );
