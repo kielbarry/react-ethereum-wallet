93bda57c76fd862083fd68939d32f486a4d6e01b
kiel barry <kiel.j.barry@gmail.com>
2018-09-28 18:26:05 -0700

    send html

--- src/components/views/send.js
+++ src/components/views/send.js
@@ -23,6 +23,81 @@ class SendContractForm extends Component {
         <h1>
           <strong>Send</strong> Funds
         </h1>
+        <div className="row clear from-to">
+          <div className="col col-6 mobile-full from">
+            <h3>From</h3>
+            <div className="dapp-select-account send-from">
+              <select name="dapp-select-account" className="send-from">
+                <option value="0x6a6964034c192ccaa8594a179db7a98ad581e4f2">
+                  🔑 Main account (Etherbase) - 0.21 USD (0.00 ETHER)
+                </option>
+              </select>
+              <span
+                className="dapp-identicon dapp-small"
+                title="This is a security icon.  If there were any change to the address, the resulting icon would be a completely different one"
+              >
+                <img src="" className="identicon-pixel" />
+              </span>
+            </div>
+          </div>
+          <div className="col col-6 mobile-full">
+            <h3>To</h3>
+            <div className="dapp-address-input">
+              <input
+                type="text"
+                name="to"
+                placeholder="0x000000.."
+                className="to"
+                autoFocus="true"
+              />
+            </div>
+          </div>
+          <div className="dapp-clear-fix" />
+        </div>
+        <div className="row clear">
+          <div className="col col-6 mobile-full amount">
+            <h3>Amount</h3>
+            <input
+              type="text"
+              min="0"
+              step="any"
+              name="amount"
+              placeholder="0.0"
+              className="dapp-large"
+              pattern="[0-9\.,]*"
+            />
+            <br />
+            <label>
+              <input type="checkbox" className="send-all" />
+              Send everything
+            </label>
+            <p className="send-info">
+              You want to send <strong>0 USD</strong> in Ether, using exchange
+              rates from
+              <a
+                href="https://www.cryptocompare.com/coins/eth/overview/BTC"
+                target="_blank"
+              >
+                {' '}
+                cryptocompare.com
+              </a>
+              .<br />
+              Which is currently an equivalent of <strong>0 ETHER</strong>.
+            </p>
+          </div>
+          <div className="col col-6 mobile-full">
+            <br />
+            <br />
+            <div className="token-ether">
+              <span className="ether-symbol">Ξ</span>
+              <span className="token-name">ETHER</span>
+              <span className="balance">
+                5,538.38 USD (26.41223000001 ETHER)
+              </span>
+            </div>
+          </div>
+          <div className="dapp-clear-fix" />
+        </div>
 
         <FormInput />
         <LatestTransactions />
