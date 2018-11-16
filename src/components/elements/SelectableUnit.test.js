390e10d9c451a11dbe8a2ffe4b691aea85512df6
kiel barry <kiel.j.barry@gmail.com>
2018-11-16 13:16:12 +0100

    praha:selectable unit snapshot

---
+++ src/components/elements/SelectableUnit.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import fetchMock from 'fetch-mock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('renders selectable unit', () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it('renders dropdown', () => {
+    let cn = require('classnames');
+    let newClasses = cn({
+      'simple-modal': true,
+      animate: false,
+    });
+    const tree = renderer
+      .create(
+        <div id="selectableUnitDrawer" className={newClasses} ref={() => {}}>
+          <ul>
+            <li key="1">
+              <button data-value="ETHER" onClick={() => {}}>
+                ETHER
+              </button>
+            </li>
+          </ul>
+        </div>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
