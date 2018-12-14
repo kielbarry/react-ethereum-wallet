88195b5fef015f27e6b9c67b05363bb715d7c67e
kiel barry <kiel.j.barry@gmail.com>
2018-12-14 19:32:18 +0100

    typo

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const NotFound = () => {
-  return <div>There doesn't seem to be anything here...</div>;
+  return <div>There doesn&apos;t seem to be anything here...</div>;
 };
 
 export default NotFound;
